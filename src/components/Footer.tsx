c328920773d9fa15f9f0a556942602bb4ea0a306
devanaclimgo <anaclimgo@gmail.com>
2025-08-25 15:48:51 -0300

    refactor: update Footer component to remove placeholder support link and adjust layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,7 @@ const Footer = () => {
     <div>
       {' '}
       {/* Footer */}
-      <div className="mt-12">
-        {/* <a     // trocar link por modal QR code do pix
-          href="https://apoia.se/trilhaclara"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-flex items-center px-6 py-3 text-sm font-medium text-primary hover:text-accent transition-colors duration-300 bg-white/60 hover:bg-white/80 backdrop-blur-sm rounded-full shadow-lg hover:shadow-xl"
-        >
-          Apoie este projeto no Apoia.se
-        </a>
-        */}
+      <div className="mt-6">
         <p className='inline-flex items-center px-6 py-3 text-sm font-medium text-gradient-trilha'>Desenvolvido por Ana Gomes | 2025</p>
       </div>
     </div>
