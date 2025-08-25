70fdfbb368c246a8097d7e94a19dd4060402ed9b
devanaclimgo <anaclimgo@gmail.com>
2025-08-25 20:51:26 -0300

    style: remove margin from Footer component for cleaner layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
       {' '}
       {/* Footer */}
-      <div className="mt-6">
+      <div>
         <p className='inline-flex items-center px-6 py-3 text-sm font-medium text-gradient-trilha'>Desenvolvido por Ana Gomes | 2025</p>
       </div>
     </div>
